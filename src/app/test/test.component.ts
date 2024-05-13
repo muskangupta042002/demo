c1f8506855e9c988dca2735613b1be70596d94dc
muskangupta042002 <muskangupta042002@gmail.com>
2024-05-14 00:10:26 +0530

    Event binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: '.app-test',
-  templateUrl: './test.component.html',
+  selector: 'app-test',
+  template: `
+  <h2>Welcome </h2>
+  <button (click)="onClick($event)">Greet</button>
+  {{greeting}}
+  `,
   styleUrls: ['./test.component.css']
 })
 export class TestComponent {
-  
+  public siteUrl=window.location.href;
+  onClick(event:any){
+    console.log("hello") 
+    console.log(event)
+    this.greeting=event.type
+  }
+  public greeting=""
 }
